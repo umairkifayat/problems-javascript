53aa59e7f18225a0c88c4912bba60616683b3199
umair kifayat <umairkifayat3@gmail.com>
2023-08-20 13:29:30 -0700

    problems-javascript

---
+++ app.js
@@ -0,0 +1,38 @@
+// <!-- question no 1 start -->
+// const myparagraph = document.getElementById("myparagraph");
+//  myparagraph.textContent="hello DOM"
+// <!-- question no 1 end -->
+
+
+
+
+
+//  question no 2 start
+
+function change_color() {
+    const para = document.querySelector("#paragraph");
+    if (para.style.backgroundColor == "red") {
+        para.style.backgroundColor = "white";
+        para.style.color = "black";
+
+    } else {
+        para.style.backgroundColor = "red";
+        para.style.color = "white";
+
+    }
+}
+// question no 2 end
+
+
+
+
+// <!-- question no 3 start -->
+
+// const hiddenText = document.getElementById("hiddenText")
+// const showTextButton = document.getElementById("showTextButton")
+
+// showTextButton.addEventListener("click", function () {
+//     hiddenText.style.display = "block";
+
+// })
+//   <!-- question no 3 end -->
